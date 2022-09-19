2d640d06d31d64fb840afd9b8ed3e80626d87b7a
Aarathy <aarathybabu907@gmail.com>
2022-09-19 14:48:09 +1000

    bug in qld data processing

--- R/interim_qld_data.R
+++ R/interim_qld_data.R
@@ -17,23 +17,23 @@ rats_30_7 <- rats %>%
 nocs_by_date <- nocs %>%
   group_by(CollectionDate) %>%
   mutate(daily_count = sum(n)) %>%
-  filter(AgeAtOnset == 0, CollectionDate >= "2022-01-06") %>%
+  filter(CollectionDate >= "2022-01-06") %>%
   select(CollectionDate, daily_count) %>%
-  ungroup() %>%
+  ungroup()  %>% distinct(CollectionDate, .keep_all = TRUE)%>%
   rename(PCR_QLD = daily_count)
 
-rats_by_date <- rats %>%
-  group_by(DateOfTestResult) %>%
-  mutate(daily_count = sum(n)) %>%
-  filter(AgeAtSubmission == 0, DateOfTestResult >= "2022-01-06") %>%
-  select(DateOfTestResult, daily_count) %>%
-  ungroup() %>%
+rats_by_date <- rats %>% 
+  group_by(DateOfTestResult) %>% 
+  mutate(daily_count = sum(n)) %>% 
+  filter(DateOfTestResult >= as_date("2022-01-06")) %>% 
+  select(DateOfTestResult,daily_count) %>% 
+  ungroup() %>% distinct(DateOfTestResult, .keep_all = TRUE)%>%
   rename(RAT_QLD = daily_count)
 
 linelist_commonwealth_QLD <- nocs_by_date %>%
   full_join(rats_by_date, by = c("CollectionDate" = "DateOfTestResult")) %>%
   rename("Date" = "CollectionDate") %>%
-  mutate(Date = as.Date(Date, format = "%Y%m%d"))
+  mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
 
 write_csv(linelist_commonwealth_QLD,
           "~/not_synced/qld/linelist_commonwealth_QLD.csv")
