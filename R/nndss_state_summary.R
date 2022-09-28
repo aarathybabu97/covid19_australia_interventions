94d2a722b16f66c3e50ad4751f11710a1e15ac32
Aarathy <aarathybabu907@gmail.com>
2022-09-28 13:45:05 +1000

    stitch up act commonwealth data for dates from 2022-01-06 & 2022-03-28

--- R/nndss_state_summary.R
+++ R/nndss_state_summary.R
@@ -2,7 +2,7 @@
 
 source("R/functions.R")
 # add queensland data 
-source("R/interim_qld_data.R")
+source("R/interim_qld_act_data.R")
 
 #overall linelist read line
 linelist <- load_linelist(use_vic = FALSE) #skip Vic since using summary data
@@ -24,7 +24,7 @@ linelist <- linelist %>% filter(date_confirmation >= "2020-01-23")
 saveRDS(
   linelist,
   sprintf(
-    "outputs/linelist_new_%s.RDS",
+    "outputs/linelist_%s.RDS",
     linelist$date_linelist[1] %>%
       format.Date(format = "%Y%m%d")
   )
@@ -47,7 +47,7 @@ vic_state_count <- read_csv(vic.files) %>% rename("PCR_VIC" = "confirmed",
                                                   "Date" = "date")
 
 #shift date to be in line with commonwealth
-vic_state_count$Date <- vic_state_count$Date+1
+#vic_state_count$Date <- vic_state_count$Date+1
 
 qld_state_count <- read_csv("~/not_synced/qld/linelist_commonwealth_QLD.csv")%>% 
   mutate(Date=as.Date(Date,format = "%Y%m%d"))
@@ -73,13 +73,38 @@ linelist_VIC_QLD <- linelist_VIC_QLD_preprocess %>%
   mutate("test_type" = word(state,1,1, sep = fixed("_")),
          "state" = word(state,2,2, sep = fixed("_"))) #%>% 
 
-
+linelist_VIC_QLD <- linelist_VIC_QLD %>%
+  filter(date_confirmation>="2022-01-06") # qld & Vic state summary from 2022-01-06
 linelist_VIC_QLD %>%
   filter(is.na(daily_notification))
 
 linelist_VIC_QLD <- linelist_VIC_QLD %>% dplyr::mutate(daily_notification = replace_na(daily_notification, 0))%>%
   
   uncount(weights = daily_notification)
+
+act_commonwealth<- read_csv("~/not_synced/qld/act_commonwealth.csv")%>% 
+  mutate(Date=as.Date(Date,format = "%Y%m%d"))%>%
+  pivot_longer(-Date,
+               names_to = "state",
+               values_to = "daily_notification") %>% 
+  mutate(
+    date_confirmation = as_date(Date)
+  ) %>%
+  arrange(
+    date_confirmation
+  )  %>%
+  select(
+    date_confirmation,
+    daily_notification,
+    state
+  ) %>% 
+  mutate("test_type" = word(state,1,1, sep = fixed("_")),
+         "state" = word(state,2,2, sep = fixed("_")))
+
+act_commonwealth<-act_commonwealth%>%
+  
+  uncount(weights = daily_notification) 
+
 
 get_latest_linelist()
 regular_ll <-linelist
@@ -97,20 +122,30 @@ old_delay_cdf <- get_notification_delay_cdf(regular_ll)
 
 #make into reff data format
 linelist_VIC_QLD$import_status <- "local"
-
+act_commonwealth$import_status <- "local"
 # linelist_new <- regular_ll %>%
 #   filter(date_confirmation >= "2022-01-06" & !state %in% c("QLD","VIC")) %>%
 #   bind_rows(linelist_VIC_QLD)
 
 # for vic use linelist till 2022/01/06 & then replace with summary
 
+# linelist_new <- linelist_VIC_QLD %>%
+#   filter(date_confirmation>="2022-01-06")%>% # qld & Vic state summary from 2022-01-06
+#   bind_rows(regular_ll%>%
+#               filter(!state %in% c("QLD","VIC")))%>%  # nindss data from beginning for all states
+#   bind_rows(regular_ll %>%
+#           filter(date_confirmation < "2022-01-06" & state %in% c("QLD","VIC"))) # bound with qld & vic before 2022-01-06
+# 
+
 linelist_new <- linelist_VIC_QLD %>%
   filter(date_confirmation>="2022-01-06")%>% # qld & Vic state summary from 2022-01-06
+  bind_rows(act_commonwealth)%>% #act commonwealth data 2022-01-06 to 2022-03-28
   bind_rows(regular_ll%>%
-              filter(!state %in% c("QLD","VIC")))%>%  # nindss data from beginning for all states
+              filter(!state %in% c("QLD","VIC","ACT")))%>%  # nindss data from beginning for all other states except these
+  bind_rows(regular_ll%>%
+              filter(state=="ACT",date_confirmation>"2022-03-28"))%>%
   bind_rows(regular_ll %>%
-          filter(date_confirmation < "2022-01-06" & state %in% c("QLD","VIC"))) # bound with qld & vic before 2022-01-06
-
+              filter(date_confirmation < "2022-01-06" & state %in% c("QLD","VIC","ACT"))) # bound with qld, act & vic before 2022-01-06
 
 
 linelist_new$interstate_import[is.na(linelist_new$interstate_import)] <- FALSE
@@ -140,22 +175,22 @@ linelist_new %>%
   ) +
   geom_vline(xintercept = linelist_new$date_linelist[1])
 
-# wa_examine <- check_wa_linelist%>%
-#   filter(state=="WA")%>%
-#   filter(date_confirmation>="2022-08-20"& date_confirmation<="2022-08-29")%>%
-#   group_by(state, date_confirmation, test_type) %>%
-#   summarise(cases = n())
-# 
-# wa_examine%>%
-#   ggplot() +
-#   geom_bar(
-#     aes(
-#       x = date_confirmation,
-#       y = cases,
-#       fill = test_type
-#     ),
-#     stat = "identity"
-#   ) 
+nt_examine <- regular_ll%>%
+  filter(state=="NT")%>%
+  filter(date_confirmation>="2022-08-25")%>%
+  group_by(state, date_confirmation, test_type) %>%
+  summarise(cases = n())
+
+nt_examine%>%
+  ggplot() +
+  geom_bar(
+    aes(
+      x = date_confirmation,
+      y = cases,
+      fill = test_type
+    ),
+    stat = "identity"
+  )
 #   geom_vline(xintercept = linelist_new$date_linelist[1])
 
 
@@ -165,14 +200,21 @@ linelist_new %>%
 #impute - this takes a very long time
 #we can choose not to impute and it will be imputed in reff_model_data function instead
 #makes no difference, but can impute first to save progress in case of issues
+
+linelist_new %>%
+  filter(date_confirmation >= Sys.Date()%m-% months(2)) %>%
+  group_by(state, date_confirmation, test_type) %>%
+  summarise(cases = n())%>%
+  arrange(desc(date_confirmation))->datacheck
+
 set.seed(2020-04-29)
-linelist_new <- linelist_new %>%
+linelist <- linelist_new %>%
   impute_linelist(notification_delay_cdf = old_delay_cdf)
 
 saveRDS(linelist,"outputs/commonwealth_ll_imputed_old_method.RDS")
 #linelist <- readRDS("outputs/commonwealth_ll_imputed.RDS")
 
-data <- reff_model_data(linelist_raw = linelist_new,
+data <- reff_model_data(linelist_raw = linelist,
                         notification_delay_cdf = old_delay_cdf)
 #data[["valid_mat"]][c(919,920),"QLD"] <- FALSE
 saveRDS(data, "outputs/pre_loaded_reff_data_old_imputation.RDS")
